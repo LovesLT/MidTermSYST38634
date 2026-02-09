fce14dfd8d1267940fa8e93b3e3e5b19a84c33ea
lovepreet <toorlo@sheridancollege.ca>
2026-02-09 17:29:09 -0500

    Initial commit by Lovepreet Toor

---
+++ src/main/java/test/productmanager/ProductManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package test.productmanager;
+
+/**
+ *
+ * @author Lp_To
+ */
+public class ProductManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
